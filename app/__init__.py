a5ad06d1d489e61e3aeb233d410a1297c24368bb
Alix Fullerton <alix@politeauthority.com>
2017-03-30 22:02:18 -0600

    Adding /gen/ to make to populate cmd line args

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask
 from flask import Response
 from flask import render_template
+from flask import request
 app = Flask(__name__)
 
 
@@ -11,14 +12,29 @@ def index():
 
 
 @app.route('/load/<script>')
+@app.route('/l/<script>')
 def bash(script):
-    """
-
-    """
     base_bash_path = './bash'
     bash_path = os.path.join(base_bash_path, script + '.sh')
     if not os.path.exists(bash_path):
         bash_path = os.path.join(base_bash_path, '404.sh')
     phile = open(bash_path, 'r')
-    data = phile.read()
-    return Response(data, mimetype='text/text')
+    bash_data = phile.read()
+    return Response(bash_data, mimetype='text/text')
+
+
+@app.route('/gen/<script>')
+def gen(script):
+    """
+
+    """
+    # shit = request.headers['Host']  # prints "domain1.com"
+    curl = "curl -sSL %s/l/%s | bash" % (request.headers['Host'], script)
+    data = {
+        'curl': curl
+    }
+    return __rend('generate_curl.html', data)
+
+
+def __rend(template, data, _type="/text/html"):
+    return Response(render_template(template, **data), _type)
