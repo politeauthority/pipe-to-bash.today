a0d602ba396bb3c071ae2fd024d35292867e6336
Alix Fullerton <alix@politeauthority>
2017-03-30 21:38:57 -0600

    Working on containerization

--- run.py
+++ run.py
@@ -1,15 +1,17 @@
 import os
+import logging
 
 deployment = os.environ.get('PTBT_DEPLOYMENT', 'production')
-
 _debug = False
-_port = 80
+_port = os.environ.get('PTBT_PORT', '80')
+
 if deployment == 'dev':
     _debug = True
     _port = 8071
 
 if __name__ == '__main__':
     from app import app
+    logging.basicConfig(filename='logs/app.log', level=logging.DEBUG)
     app.run(
         host="0.0.0.0",
         port=_port,
