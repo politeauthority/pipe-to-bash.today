5059b9fc15eea5f2967f9bd155bdcf7a5b078665
Alix Fullerton <alix@politeauthority>
2017-03-30 20:42:14 -0600

    Changing docker run?

---
+++ resources/docker_run.sh
@@ -0,0 +1,13 @@
+docker stop pipe_to_bash
+docker rm pipe_to_bash
+docker run \
+       --name pipe_to_bash \
+       -e DB_USER="" \
+       -e DB_PASS="" \
+       -e WP_DB_NAME="" \
+       -e WP_DB_HOST="" \
+       -e VIRTUAL_HOST="pipe-to-bash.today" \
+       -e PTBT_DEPLOYMENT="dev" \
+       -d \
+       --restart=always \
+       pipe-to-bash:latest
